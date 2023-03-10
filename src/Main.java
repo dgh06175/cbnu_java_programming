f9d12c8195eb75ccd2528935d0a5bcf5624cba2e
dgh06175 <dgh06175@gmail.com>
2023-03-10 14:27:47 +0900

    첫날 수업

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.sql.Driver;
+
 public class Main {
     public static void main(String[] args) {
+        int i = 1;
+        i++;
+        i++;
+        i++;
+        i++;
         System.out.println("Hello world!");
+        Main main = new Main();
+        Car car = new Car();
     }
 }
